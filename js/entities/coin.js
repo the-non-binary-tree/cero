2f5ca1a9bb371600705077691bc4f2f8eb681a7d
Lilian Zhao <milanzq309@gmail.com>
2021-06-18 22:28:20 -0700

    player movmt and controls and map changes

---
+++ js/entities/coin.js
@@ -0,0 +1,34 @@
+// DC
+game.CoinEntity = me.Sprite.extend({
+    
+    init: function (x, y, settings) {
+        this._super(me.Sprite, "init", [
+            x, y ,
+            Object.assign({
+                image: game.texture,
+                region : "collectables/coin"
+            }, settings)
+        ]);
+
+        this.body = new me.Body(this, new me.Ellipse(this.width / 2, this.height / 2, this.width, this.height));
+        this.body.collisionType = me.collision.types.COLLECTABLE_OBJECT;
+
+        this.anchorPoint.set(.1, .4);
+    },
+
+    // player touch
+    onCollision : function (/*response*/) {
+
+        // do something when collide
+        me.audio.play("cling", false);
+        // give some score
+        game.data.score += 5;
+
+        // delete object
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+
+        me.game.world.removeChild(this);
+
+        return false;
+    }
+});
--- js/entities/player.js
+++ js/entities/player.js
@@ -1,26 +1,30 @@
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings) {
         this._super(me.Entity, "init", [x, y , settings]);
-        this.alwaysUpdate = true; //exit viewport
-
-        //scrollable background
-        me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 0.1);
-
-        // on keypress
-        me.input.bindKey(me.input.KEY.LEFT,  "left");
-        me.input.bindKey(me.input.KEY.RIGHT, "right");
-        me.input.bindKey(me.input.KEY.A,     "left");
-        me.input.bindKey(me.input.KEY.D,     "right");
-        me.input.bindKey(me.input.KEY.W,     "jump", true);
-        me.input.bindKey(me.input.KEY.UP,    "jump", true);
-        me.input.bindKey(me.input.KEY.SPACE, "jump", true);
+        this.alwaysUpdate = true;
 
         // walking & jumping speed
-        this.body.setMaxVelocity(3, 15);
+        this.body.setMaxVelocity(3, 18);
         this.body.setFriction(0.4, 0);
 
         this.dying = false;
         this.mutipleJump = 1;
+
+        // scrollable screen
+        me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 0.1);
+
+        // onkeydown
+        me.input.bindKey(me.input.KEY.LEFT,  "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.X,     "jump", true);
+        me.input.bindKey(me.input.KEY.UP,    "jump", true);
+        me.input.bindKey(me.input.KEY.SPACE, "jump", true);
+        me.input.bindKey(me.input.KEY.DOWN,  "down");
+
+        me.input.bindKey(me.input.KEY.A,     "left");
+        me.input.bindKey(me.input.KEY.D,     "right");
+        me.input.bindKey(me.input.KEY.W,     "jump", true);
+        me.input.bindKey(me.input.KEY.S,     "down");
 
         // set a renderable
         this.renderable = game.texture.createAnimationFromName([
@@ -28,16 +32,14 @@ game.PlayerEntity = me.Entity.extend({
             "character/redSquare4",
         ]);
 
-        // define a basic walking animation
+        // mvmt anim
         this.renderable.addAnimation ("walk",  [{ name: "character/redSquare1", delay: 200 }, { name: "character/redSquare2", delay: 200 }, { name: "character/redSquare3", delay: 200 }, { name: "character/redSquare4", delay: 200 }, { name: "character/redSquare3", delay: 200 }, { name: "character/redSquare2", delay: 200 } ]);
-        // set as default
         this.renderable.setCurrentAnimation("walk");
 
-        // change renderable position
-        this.anchorPoint.set(.5, 0.5);
+        this.anchorPoint.set(.5, 0.4);
     },
 
-    // player position 
+    // position update
     update : function (dt) {
 
         if (me.input.isKeyPressed("left"))    {
@@ -52,12 +54,41 @@ game.PlayerEntity = me.Entity.extend({
 
         if (me.input.isKeyPressed("jump")) {
             this.body.jumping = true;
+            if (this.multipleJump <= 2) {
+                this.body.force.y = -this.body.maxVel.y * this.multipleJump++;
+                me.audio.stop("jump");
+                me.audio.play("jump", false);
+            }
+        }
+        else {
+
+            this.body.force.y = 0;
+
+            // reset mjump
+            if (!this.body.falling && !this.body.jumping) {
+                this.multipleJump = 1;
+            }
+            else if (this.body.falling && this.multipleJump < 2) {
+                this.multipleJump = 2;
+            }
+        }
+
+        this.body.update(dt);
+
+        // check if exit viewport by falling
+        if (!this.inViewport && (this.pos.y > me.video.renderer.getHeight())) {
+            // reset
+            me.game.world.removeChild(this);
+            me.game.viewport.fadeIn("#fff", 150, function(){
+                me.audio.play("die", false);
+                me.levelDirector.reloadLevel();
+                me.game.viewport.fadeOut("#fff", 150);
+            });
+            return true;
         }
 
         // handle collisions against other shapes
         me.collision.check(this);
-
-        // check if we moved (an "idle" animation would definitely be cleaner)
         if (this.body.vel.x !== 0 || this.body.vel.y !== 0 ||
             (this.renderable && this.renderable.isFlickering())
         ) {
@@ -68,48 +99,40 @@ game.PlayerEntity = me.Entity.extend({
         return false;
     },
 
-
-    // collisions
     onCollision : function (response, other) {
         switch (other.body.collisionType) {
             case me.collision.types.WORLD_SHAPE:
-                // Simulate a platform object
+                // platform sim
                 if (other.type === "platform") {
                     if (this.body.falling &&
                         !me.input.isKeyPressed("down") &&
-                        // Shortest overlap would move the player upward
                         (response.overlapV.y > 0) &&
-                        // The velocity is reasonably fast enough to have penetrated to the overlap depth
+                        // check velocity fast enough
                         (~~this.body.vel.y >= ~~response.overlapV.y)
                     ) {
-                        // Disable collision on the x axis
+                        // is solid
                         response.overlapV.x = 0;
-                        // Repond to the platform (it is solid)
                         return true;
                     }
-                    // Do not respond to the platform (pass through)
+                    // pass thru
                     return false;
                 }
 
-                // Custom collision response for slopes
+                // custom collision response for slopes
                 else if (other.type === "slope") {
-                    // Always adjust the collision response upward
+                    // adjust response upward
                     response.overlapV.y = Math.abs(response.overlap);
                     response.overlapV.x = 0;
-
-                    // Respond to the slope (it is solid)
                     return true;
                 }
                 break;
 
             case me.collision.types.ENEMY_OBJECT:
                 if (!other.isMovingEnemy) {
-                    // spike or any other fixed danger
                     this.body.vel.y -= this.body.maxVel.y * me.timer.tick;
                     this.hurt();
                 }
                 else {
-                    // a regular moving enemy entity
                     if ((response.overlapV.y > 0) && this.body.falling) {
                         // jump
                         this.body.vel.y -= this.body.maxVel.y * 1.5 * me.timer.tick;
@@ -117,17 +140,14 @@ game.PlayerEntity = me.Entity.extend({
                     else {
                         this.hurt();
                     }
-                    // Not solid
                     return false;
                 }
                 break;
 
             default:
-                // Do not respond to other objects (e.g. coins)
+                // other objects: no resp
                 return false;
         }
-
-        // Make the object solid
         return true;
     },
 
@@ -148,4 +168,4 @@ game.PlayerEntity = me.Entity.extend({
             me.audio.play("die", false);
         }
     }
-})
+});
