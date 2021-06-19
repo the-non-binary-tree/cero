f44c7f1be7784e0a161e7ffa0a43ca0e8532a297
Lilian Zhao <milanzq309@gmail.com>
2021-06-18 17:53:54 -0700

    worked on sprites and tileset

---
+++ data/map/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="tileset1" tilewidth="80" tileheight="80" tilecount="100" columns="10">
+ <image source="../../../tilesets/tileset1.png" width="800" height="800"/>
+</tileset>
