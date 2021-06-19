7f8e2096553b7ea7cfe9769ccf8093f896d40d6b
Lilian Zhao <milanzq309@gmail.com>
2021-06-19 15:09:05 -0700

    implemented stats menu

--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+    // title screen trigger
     document.querySelector('#begin-btn').addEventListener('click', () => {
         let title = document.querySelector('#title-screen')
         title.style.opacity = '0%';
@@ -8,9 +9,31 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 1000);
     });
 
+    // menu display
+    let menuOn = false
+    document.querySelector('#menu-btn').addEventListener('click', () => {
+        let menu = document.querySelector('#menu')
+        if (menuOn) {
+            menu.classList.remove('fadeIn');
+            menu.classList.add('fadeOut');
+            setTimeout(() => {
+                menu.style.display = 'none';
+            }, 1000);
+            menuOn = false;
+        } else {
+            menu.style.display = 'flex';
+            menu.classList.remove('fadeOut');
+            menu.classList.add('fadeIn');
+            menuOn = true;
+        }
+    });
+
+    // close speech box
     document.querySelector('#ok-btn').addEventListener('click', () => {
         let speechBox = document.querySelector('#speech-container')
         speechBox.classList.remove('fadeIn')
         speechBox.classList.add('fadeOut')
     });
+
+
 });
